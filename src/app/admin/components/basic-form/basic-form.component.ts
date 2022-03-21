0930b3f026657610aaa637b5e747644336699924
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2022-03-21 13:33:09 -0700

    usando FormGroup para grupar multiples campos

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -8,19 +8,33 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
-  emailField = new FormControl('');
-  phoneField = new FormControl('');
-  colorField = new FormControl('#000000');
-  dateField = new FormControl('');
-  ageField = new FormControl('');
+  form= new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    color: new FormControl('#000000'),
+    date: new FormControl(''),
+    age: new FormControl(''),
+    category: new FormControl(''),
+    tag: new FormControl(''),
+    agree: new FormControl('false'),
+    gender: new FormControl(''),
+    zone: new FormControl(''),
+  });
 
-  categoryField = new FormControl('');
-  tagField = new FormControl('');
+  // nameField = new FormControl('');
+  // emailField = new FormControl('');
+  // phoneField = new FormControl('');
+  // colorField = new FormControl('#000000');
+  // dateField = new FormControl('');
+  // ageField = new FormControl('');
 
-  agreeField = new FormControl(false);
-  genderField = new FormControl('');
-  zoneField = new FormControl('');
+  // categoryField = new FormControl('');
+  // tagField = new FormControl('');
+
+  // agreeField = new FormControl(false);
+  // genderField = new FormControl('');
+  // zoneField = new FormControl('');
 
   constructor() { }
 
@@ -31,8 +45,57 @@ export class BasicFormComponent implements OnInit {
     })
   }
 
+  save(event) {
+    console.log(this.form.value);
+
+  }
+
   getNameValue() {
     console.log(this.nameField.value);
+  }
+
+  get nameField() {
+    return this.form.get('name')
+  }
+
+  get emailField() {
+    return this.form.get('email')
+  }
+
+  get phoneField() {
+    return this.form.get('phone')
+  }
+
+  get colorField() {
+    return this.form.get('color')
+  }
+
+  get dateField() {
+    return this.form.get('date')
+  }
+
+  get ageField() {
+    return this.form.get('age')
+  }
+
+  get categoryField() {
+    return this.form.get('category')
+  }
+
+  get tagField() {
+    return this.form.get('tag')
+  }
+
+  get agreeField() {
+    return this.form.get('agree')
+  }
+
+  get genderField() {
+    return this.form.get('gender')
+  }
+
+  get zoneField() {
+    return this.form.get('zone')
   }
 
   get isNameFieldValid() {
