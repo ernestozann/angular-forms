35b2d7e198ee3a5c5701d335fbddec9f4064d24d
Ernesto Martinez <ernestomm99@hotmail.com>
2022-03-30 12:05:32 -0700

    crea tu propia libreria de componentes con CVA o control Value Accesor

--- src/app/admin/products/products.module.ts
+++ src/app/admin/products/products.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from './../../material/material.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './components/products/products.component';
@@ -21,7 +22,8 @@ import { ProductEditComponent } from './components/product-edit/product-edit.com
     CommonModule,
     ReactiveFormsModule,
     MaterialModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    SharedModule,
   ]
 })
 export class ProductsModule { }
