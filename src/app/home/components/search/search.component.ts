8b59c6cbf2e2353971083d885bf6a827893bce1b
Ernesto Martinez <ernestomm99@hotmail.com>
2022-04-01 10:38:26 -0700

    Optimiza un input de busquedas con RxJS y debounce

--- src/app/home/components/search/search.component.ts
+++ src/app/home/components/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http'
-import { map } from 'rxjs/operators';
+import { map, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -19,6 +19,7 @@ export class SearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.searchField.valueChanges
+    .pipe(debounceTime(300))
     .subscribe( value => {
       this.getData(value)
     })
@@ -33,7 +34,7 @@ export class SearchComponent implements OnInit {
       })
     )
     .subscribe((data) => {
-      console.log(data);
+      this.results = data
     })
   }
 
